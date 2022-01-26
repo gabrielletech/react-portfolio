36c7873743a07e7035ec9f4f82eb31bd66f66fc9
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-01-26 20:54:31 +0200

    Homepage - Mobile + Desktop

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,19 +1,35 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
-import Dropdown from 'react-bootstrap/Dropdown'
 import Footer from '../../components/footer/Footer'
+import HomeImg from '../../images/home-mockup.png'
+import { Link } from 'react-router-dom'
 
 import classes from './Home.module.scss'
 
 export default function Home() {
+
   return (
       <>
       <section>
         <div className={classes.hero}>
-           <Navbar />
+        <div className={classes.nav}>
+          <Navbar />
+        </div>
             <div className={classes.hero_text}>
               <h1 className={classes.hero_title}>Gabrielle Ndoro</h1>
               <h4 className={classes.sub_title}>Freelance Web Designer & Front-End Developer</h4>
+              <button className={classes.hero_btn}>
+              <Link to='/contact' className={classes.link}>
+                   Get In Touch
+              </Link>
+             </button>
+            </div>
+            <div className={classes.hero_side}>
+              <img src={HomeImg} alt="Responsive Web Design" className={classes.hero_img} />
+              <div className={classes.side_text}>
+                I help Small Businesses and Entrepreneurs build an online presence - giving you
+                 a website that represents your brand, and that your users will LOVE!
+              </div>
             </div>
         </div>
 
