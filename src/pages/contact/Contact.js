6db4f6e72044c34aaf3a0bdd70c9155c92b6d613
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-02-03 21:56:02 +0200

    Modified

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState} from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
+import Alert from 'react-bootstrap/Alert'
 
 import classes from './Contact.module.scss'
 
 export default function Contact() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [emailSent, setEmailSent] = useState(false)
+
+  const submit = () => {
+    if (name && email && message) {
+      setName('')
+      setEmail('')
+      setMessage('')
+      setEmailSent(true)
+    }
+    else {
+      <Alert variant="secondary">'Please fill in all fields.'</Alert>
+    }
+  }
+
+  // form validation
+  const isValidEmail = email => {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email).toLowerCase());
+};
+
   return (
     <>
       <section>
@@ -26,11 +50,28 @@ export default function Contact() {
           </div>
 
           <div className={classes.contact_form} id="contact-form">
-            <input type="text" placeholder="Your Name" className={classes.input_name}/>
-            <input type="email" placeholder="Your email address" className={classes.input_email}/>
-            <textarea placeholder="Your message" className={classes.input_message}></textarea>
-            <button className={classes.submit_btn}>Send Message</button>
-            <span className={classes.thank_you_msg}>Thank you for your message, we will be in touch in no time!</span>
+            <input 
+                type="text" 
+                placeholder="Your Name" 
+                className={classes.input_name} 
+                value={name}
+                onChange={e => setName(e.target.value)}
+                
+            />
+            <input 
+                type="email" 
+                placeholder="Your email address" 
+                className={classes.input_email}
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+            />
+            <textarea 
+                placeholder="Your message" 
+                className={classes.input_message}
+                value={message}
+                onChange={e => setMessage(e.target.value)}></textarea>
+            <button className={classes.submit_btn} onClick={submit}>Send Message</button>
+            <span className={emailSent ? 'visible' : null}>Thank you for your message, we will be in touch in no time!</span>
           </div>
         </div>
       </section>
