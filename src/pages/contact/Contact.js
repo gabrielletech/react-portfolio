c302876c30f975fc770c9b424379d0714599b909
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-02-14 19:08:12 +0200

    Modified

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,41 +1,12 @@
-import React, { useState} from 'react'
+import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
-import Alert from 'react-bootstrap/Alert'
-import emailjs from 'emailjs-com'
+//import Alert from 'react-bootstrap/Alert'
+//import emailjs from 'emailjs-com'
 
 import classes from './Contact.module.scss'
 
 export default function Contact() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
-  const [emailSent, setEmailSent] = useState(false)
-
-  const submit = () => {
-    if (name && email && message) {
-      const serviceId = 'service_id';
-            const templateId = 'template_id';
-            const userId = 'user_id';
-            const templateParams = {
-                name,
-                email,
-                message
-            };
-
-            emailjs.send(serviceId, templateId, templateParams, userId)
-                .then(response => console.log(response))
-                .then(error => console.log(error));
-
-      setName('')
-      setEmail('')
-      setMessage('')
-      setEmailSent(true)
-    }
-    else {
-      <Alert variant="secondary">'Please fill in all fields.'</Alert>
-    }
-  }
 
   return (
     <>
@@ -58,29 +29,25 @@ export default function Contact() {
           </div>
 
           <div className={classes.contact_form} id="contact-form">
-            <input 
-                type="text" 
-                placeholder="Your Name" 
-                className={classes.input_name} 
-                value={name}
-                onChange={e => setName(e.target.value)}
-                
-            />
-            <input 
-                type="email" 
-                placeholder="Your email address" 
-                className={classes.input_email}
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-            />
-            <textarea 
-                placeholder="Your message" 
-                className={classes.input_message}
-                value={message}
-                onChange={e => setMessage(e.target.value)}></textarea>
-            <button className={classes.submit_btn} onClick={submit}>Send Message</button>
-            <br />
-            <span className={emailSent ? 'Show' : null}>Thank you for your message, I will get back to you soon :) </span>
+            <form target="_blank" action="https://formsubmit.co/c7e54dafc96e1d38ef4537108ff79656" method="POST">
+                  <input 
+                  type="text" 
+                  placeholder="Your Name" 
+                  className={classes.input_name} 
+                  
+              />
+              <input 
+                  type="email" 
+                  placeholder="Your email address" 
+                  className={classes.input_email}
+              />
+              <textarea 
+                  placeholder="Your message" 
+                  className={classes.input_message}
+                  ></textarea>
+              <button type="submit" className={classes.submit_btn} >Send Message</button>
+              <br />
+            </form>
           </div>
         </div>
       </section>
--- src/pages/services/Services.js
+++ src/pages/services/Services.js
@@ -10,6 +10,29 @@ import classes from './Services.module.scss'
 export default function Services() {
   AOS.init()
 
+  AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
   return (
     <>
       <section>
@@ -31,6 +54,10 @@ export default function Services() {
               <h3 className={classes.services_heading}>Here's What I Can Do For You:</h3>
           </div>
           <ul>
+          <div data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out"
+          >
             <li className={classes.box}>
                 <div className={classes.dot}>
                   <span className={classes.dot_i}></span>
@@ -39,7 +66,11 @@ export default function Services() {
                 <p className={classes.services_desc}>Affordable website solutions that will showcase your brand in the most engaging way, 
                 and market your service offering to a larger audience. Custom coded solutions available as well, bespoke and unique to you.</p>
             </li>
-            <li className={classes.box}>
+          </div>
+          <div data-aos="fade-up"
+                data-aos-duration="2000"
+                data-aos-easing="ease-in-out">
+              <li className={classes.box}>
                 <div className={classes.dot}>
                     <span className={classes.dot_iii}></span>
                 </div>
@@ -47,7 +78,11 @@ export default function Services() {
                 <p className={classes.services_desc}>An e-commerce solution designed according to your brand and
                  specifications, providing your users with a straightforward and secure shopping experience, <em>hello more sales :)</em></p>
             </li>
-            <li className={classes.box}>
+          </div>
+          <div data-aos="fade-up"
+                data-aos-duration="3000"
+                data-aos-easing="ease-in-out">
+              <li className={classes.box}>
                 <div className={classes.dot}>
                     <span className={classes.dot_iii}></span>
                 </div>
@@ -55,6 +90,7 @@ export default function Services() {
                 <p className={classes.services_desc}>Optimize your site to be highly ranked on Google, giving your brand the credibility it 
                 deserves and making sure you remain relevant and visible on search results pages like Google or Bing.</p>
             </li>
+          </div>
           </ul>
         </div> 
 
