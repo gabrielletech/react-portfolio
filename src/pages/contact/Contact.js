15810a77ab3778760e9c47177dabd8f4417260d4
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-02-23 21:42:07 +0200

    EmailJs Configured

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useRef, setState } from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
 //import Alert from 'react-bootstrap/Alert'
-//import emailjs from 'emailjs-com'
+import emailjs from '@emailjs/browser'
+import{ init } from '@emailjs/browser';
+import configData from '../../config.json'
 
 import classes from './Contact.module.scss'
 
+const service_id = configData.SERVICE_ID;
+const template_id = configData.TEMPLATE_ID;
+const user_id = configData.USER_ID;
+
 export default function Contact() {
+  const form = useRef();
+
+  init("user_nkh12fcJa28oNOnWHLj5J");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(service_id, template_id, form.current, user_id)
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text)
+      });
+
+      alert("Email Sent!")
+  };
 
   return (
     <>
@@ -21,29 +43,31 @@ export default function Contact() {
             
             <p>
               For general questions, inquiries, or technical issues with this website, drop me an email here
-              <a href='mailto:fadzisondoro.gn@gmail.com'> Gabrielle Ndoro</a>
+              <a href='mailto:gabriellendoro.fn@gmail.com' className={classes.email_address}> gabriellendoro.fn@gmail.com </a>
             </p>
             <p>Or quickly write me a message using the form. Don’t hesitate to reach out to me, besides, my consultations are completely free!</p>
           </div>
 
           <div className={classes.contact_form} id="contact-form">
-            <form target="_blank" action="https://formsubmit.co/c7e54dafc96e1d38ef4537108ff79656" method="POST">
+            <form ref={form} onSubmit={sendEmail}>
                   <input 
                   type="text" 
                   placeholder="Your Name" 
                   className={classes.input_name} 
-                  
+                  name="name"
               />
               <input 
                   type="email" 
                   placeholder="Your email address" 
                   className={classes.input_email}
+                  name="email"
               />
               <textarea 
                   placeholder="Your message" 
                   className={classes.input_message}
+                  name="message"
                   ></textarea>
-              <button type="submit" className={classes.submit_btn} >Send Message</button>
+              <input type="submit" value="Send Message" onClick={sendEmail} className={classes.submit_btn} />
               <br />
             </form>
           </div>
