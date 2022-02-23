ac048a11bc6c4b8e54f3fef69f27435bbbe924d2
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-02-23 22:11:33 +0200

    modified

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,7 +1,7 @@
-import React, { useRef, setState } from 'react'
+import React, { useRef, useState } from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
-//import Alert from 'react-bootstrap/Alert'
+import Alert from 'react-bootstrap/Alert'
 import emailjs from '@emailjs/browser'
 import{ init } from '@emailjs/browser';
 import configData from '../../config.json'
@@ -13,6 +13,9 @@ const template_id = configData.TEMPLATE_ID;
 const user_id = configData.USER_ID;
 
 export default function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
   const form = useRef();
 
   init("user_nkh12fcJa28oNOnWHLj5J");
@@ -22,12 +25,11 @@ export default function Contact() {
 
     emailjs.sendForm(service_id, template_id, form.current, user_id)
       .then((result) => {
+          <Alert variant='success'>Thank you for your message. I will get back to you as soon as I can :)</Alert>
         console.log(result.text);
       }, (error) => {
         console.log(error.text)
       });
-
-      alert("Email Sent!")
   };
 
   return (
@@ -55,17 +57,23 @@ export default function Contact() {
                   placeholder="Your Name" 
                   className={classes.input_name} 
                   name="name"
+                  value={name}
+                  onChange={(e) => e.target.value}
               />
               <input 
                   type="email" 
                   placeholder="Your email address" 
                   className={classes.input_email}
                   name="email"
+                  value={email}
+                  onChange={(e) => e.target.value}
               />
               <textarea 
                   placeholder="Your message" 
                   className={classes.input_message}
                   name="message"
+                  value={message}
+                  onChange={(e) => e.target.value}
                   ></textarea>
               <input type="submit" value="Send Message" onClick={sendEmail} className={classes.submit_btn} />
               <br />
